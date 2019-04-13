0e0892b39fd3738f46dc95d8e84d61a83945b08f
kokomin <hser0001@rangers.uwp.edu>
2019-04-12 20:02:54 -0500

    Initial Commit

---
+++ EMovies/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EMovies
+//
+//  Created by Eh Hser on 4/12/19.
+//  Copyright © 2019 Acer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
